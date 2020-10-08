5d0d4c796952aa68f75a5e4d0f6c5cdb304839d4
JohnMcCarroll <jtm5356@rit.edu>
2020-10-08 16:11:09 -0400

    created crop.py for testing mosaic cropping options

---
+++ AI/src/crop.py
@@ -0,0 +1,59 @@
+from PIL import Image
+import rasterio as rio
+import numpy as np
+from matplotlib import pyplot
+
+
+## Training Specs
+input_height = 254
+input_width = 254
+
+## load in tif mosaic
+mosaic_dataset = rio.open(r"D:\Linux Laptop\Classes\Senior Project\car.tif")
+
+## display attrs
+print("Number of bands:")
+print(mosaic_dataset.count)
+
+print("Width and height:")
+width = mosaic_dataset.width
+height = mosaic_dataset.height
+print(width)
+print(height)
+
+print("Band properties:")
+print({i: dtype for i, dtype in zip(mosaic_dataset.indexes, mosaic_dataset.dtypes)})
+
+# view image (bands)
+print("Reading Mosaic Data")
+for i in range(1,mosaic_dataset.count + 1):
+    array = None
+    array = mosaic_dataset.read(i)
+    pyplot.imshow(array, cmap='pink')
+    pyplot.show()
+
+## crop mosaic loop
+
+x_iter = round( width / input_width)
+y_iter = round(height / input_height)
+start_x = 0
+start_y = 0
+end_x = input_width
+end_y = input_height
+
+for i in range(x_iter):
+    for j in range(y_iter):
+
+        # crop
+        sample = mosaic_dataset.crop(start_x, start_y, end_x, end_y)
+
+        # annotate
+
+        # save
+        sample.save(str(i) + str(j) + ".tif")
+
+        # iterate
+        start_x += input_width 
+        end_x += input_width
+        start_y += input_height
+        end_y += input_height
