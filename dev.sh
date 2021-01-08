b691fac096bf9ef3f9cf4360b4169db62a2e2a8a
Jonathan Eid <jonathaneid98@gmail.com>
2021-01-08 08:02:58 -0500

    Uploading

---
+++ dev.sh
@@ -0,0 +1,5 @@
+# PHP Server Doc: https://www.php.net/manual/en/features.commandline.webserver.php
+
+php -S localhost:5000 -t www/ &
+php scripts/tile_daemon.php &
+npm start --prefix ourepository_react/
---
+++ ourepository_react/src/services/api.js
@@ -0,0 +1,15 @@
+const fetch = require('node-fetch');
+
+class ApiService {
+
+    sampleRequest(){
+
+    }
+
+}
+
+const apiService = new ApiService()
+
+export default apiService 
+
+ 
