02dfeeef445f47eb7fa5b1f6d9d6d6dfba403c72
Jonathan Eid <jonathaneid98@gmail.com>
2021-01-30 15:16:41 -0500

    More changes

--- www/settings.php
+++ www/settings.php
@@ -1,8 +1,7 @@
 <?php
 //ourepository:
 $CLIENT_ID = "345";
-$BASE_DIRECTORY = "./mosaic_uploads/"; 
-$UPLOAD_DIRECTORY = "./mosaic_uploads/";
-$ARCHIVE_DIRECTORY = "./mosaics/";
-
+$BASE_DIRECTORY = "./mosaics/"; 
+$UPLOAD_DIRECTORY = "./mosaics/uploads";
+$ARCHIVE_DIRECTORY = "./mosaics/archives";
 ?>
